beaf8fdc18774781fc0dc3ec43bbf543573604d5
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-10-07 06:04:07 -0400

    Issues With Translating Boards

--- scripts/Board.js
+++ scripts/Board.js
@@ -1,56 +1,58 @@
+const WIDTH = window.innerWidth;
+const PROGRAM_WIDTH = WIDTH > 700 ? WIDTH / 2 : WIDTH;
+
 class Board {
- constructor(title, color, description, index, DOMNode) {
-  this.title = title;
-  this.color = color;
-  this.description = description;
-  this.index = index;
-  this.DOMNode = DOMNode;
-  this.children = [];
-  this.dragging = false;
-  this.xOffset = 0;
- }
- 
- findTask(uid) {
-  for (let i = 0; i < this.children.length; i++) {
-   if (this.children[i].id === uid) {
-    return this.children[i];
-   }
+  constructor(title, color, description, index, DOMNode) {
+    this.title = title;
+    this.color = color;
+    this.description = description;
+    this.index = index;
+    this.DOMNode = DOMNode;
+    this.children = [];
+    this.dragging = false;
+    this.xOffset = 0;
   }
-  return null;
- }
- 
- draw() {
-  const boardsHolder = document.getElementById("boards");
-  const width = window.innerWidth;
-  this.DOMNode.style.width = `${width}px`;
-  this.DOMNode.style.minWidth = `${width - 50}px`;
-  this.DOMNode.innerHTML = this.genHtml();
-  boardsHolder.appendChild(this.DOMNode);
-  for (let i = 0; i < this.children.length; i++) {
-   this.children[i].draw();
-   this.children[i].madeRoom = false
+
+  findTask(uid) {
+    for (let i = 0; i < this.children.length; i++) {
+      if (this.children[i].id === uid) {
+        return this.children[i];
+      }
+    }
+    return null;
   }
- }
- 
- move(e) {
-  const x = e.clientX;
-  this.DOMNode.style.transform = `translateX(${x - this.xOffset}px)`;
- }
- 
- swapChildren(i, j) {
-  [this.children[i], this.children[j]] = [this.children[j], this.children[i]];
- }
- 
- removeChildTask(index) {
-  this.DOMNode.removeChild(this.children[index].DOMNode)
- }
- 
- init() {
-  this.DOMNode = document.createElement("div");
- }
- 
- genHtml() {
-  const html = `
+
+  draw() {
+    const boardsHolder = document.getElementById("boards");
+    this.DOMNode.style.width = `${PROGRAM_WIDTH - 45}px`;
+    this.DOMNode.style.minWidth = `${PROGRAM_WIDTH - 45}px`;
+    this.DOMNode.style.maxWidth = `${PROGRAM_WIDTH - 45}px`;
+    this.DOMNode.innerHTML = this.genHtml();
+    boardsHolder.appendChild(this.DOMNode);
+    for (let i = 0; i < this.children.length; i++) {
+      this.children[i].draw();
+      this.children[i].madeRoom = false;
+    }
+  }
+
+  move(x, kanbanIndex) {
+    this.DOMNode.style.transform = `translateX(${x - this.xOffset}px)`;
+  }
+
+  swapChildren(i, j) {
+    [this.children[i], this.children[j]] = [this.children[j], this.children[i]];
+  }
+
+  removeChildTask(index) {
+    this.DOMNode.removeChild(this.children[index].DOMNode);
+  }
+
+  init() {
+    this.DOMNode = document.createElement("div");
+  }
+
+  genHtml() {
+    const html = `
     <div class="board" style="background-color: ${this.color}">
       <button class="add-task-btn">+</button>
       <p class="board-drag-icon">|||</p>
@@ -63,8 +65,8 @@ class Board {
      <div class="task-items"></div>
    </div>
   `;
-  return html;
- }
+    return html;
+  }
 }
 
 export default Board;
--- scripts/Kanban.js
+++ scripts/Kanban.js
@@ -1,83 +1,88 @@
 import Board from "./Board.js";
 
+const WIDTH = window.innerWidth;
+const PROGRAM_WIDTH = WIDTH > 700 ? WIDTH / 2 : WIDTH;
+
 class Kanban {
- constructor(boards = [], tasks = []) {
-  this.boards = boards;
-  this.tasks = tasks;
-  this.index = 0;
-  this.moved = { left: false, right: false };
-  this.drawBoards();
- }
- 
- findBoard(boardIndex) {
-  const board = this.boards[boardIndex];
-  if (board) {
-   return board;
+  constructor(boards = [], tasks = []) {
+    this.boards = boards;
+    this.tasks = tasks;
+    this.index = 0;
+    this.moved = { left: false, right: false };
+    this.drawBoards();
   }
-  return null;
- }
- 
- checkSwap(x, dragBoardIndex) {
-  const leftBoard = this.findBoard(dragBoardIndex - 1);
-  const rightBoard = this.findBoard(dragBoardIndex + 1);
-  if (x < 50) {
-   if (leftBoard) {
-    leftBoard.DOMNode.style.transition = `300ms ease-in-out`;
-    leftBoard.DOMNode.style.transform = `translateX(calc(100% + 25px))`;
-    this.moved.left = true;
-   }
+
+  findBoard(boardIndex) {
+    const board = this.boards[boardIndex];
+    if (board) {
+      return board;
+    }
+    return null;
   }
-  if (x > window.innerWidth - 50) {
-   if (rightBoard) {
-    rightBoard.DOMNode.style.transition = `300ms ease-in-out`;
-    rightBoard.DOMNode.style.transform = `translateX(calc(-100% - 25px))`;
-    this.moved.right = true;
-   }
+
+  checkSwap(x, dragBoardIndex, xOffset) {
+    // const travel = x - xOffset;
+    // const leftBoard = this.findBoard(dragBoardIndex - 1);
+    // const rightBoard = this.findBoard(dragBoardIndex + 1);
+    // if (Math.abs(travel) < PROGRAM_WIDTH / 2) {
+    //   if (this.moved.left && leftBoard) {
+    //     leftBoard.DOMNode.style.transform = `translateX(0)`;
+    //     this.moved.left = false;
+    //   }
+    //   if (this.moved.right && rightBoard) {
+    //     rightBoard.DOMNode.style.transform = `translateX(0)`;
+    //     this.moved.right = false;
+    //   }
+    //   return;
+    // }
+    // if (travel < 0) {
+    //   if (leftBoard) {
+    //     leftBoard.DOMNode.style.transition = `300ms ease-in-out`;
+    //     leftBoard.DOMNode.style.transform = `translateX(calc(100% + 25px))`;
+    //     this.moved.left = true;
+    //   }
+    // }
+    // if (x > 0) {
+    //   if (rightBoard) {
+    //     rightBoard.DOMNode.style.transition = `300ms ease-in-out`;
+    //     rightBoard.DOMNode.style.transform = `translateX(calc(-100% - 25px))`;
+    //     this.moved.right = true;
+    //   }
+    // }
   }
-  if (x <= window.innerWidth - 50 && x >= 50) {
-   if (this.moved.left && leftBoard) {
-    leftBoard.DOMNode.style.transform = `translateX(0)`;
-    this.moved.left = false;
-   }
-   if (this.moved.right && rightBoard) {
-    rightBoard.DOMNode.style.transform = `translateX(0)`;
-    this.moved.right = false;
-   }
+
+  resetStyles(index1, index2) {
+    this.boards[index1].DOMNode.style.transition = "none";
+    this.boards[index2].DOMNode.style.transition = "none";
+    this.boards[index1].DOMNode.style.transform = "translateX(0)";
+    this.boards[index2].DOMNode.style.transform = "translateX(0)";
   }
- }
- 
- resetStyles(index1, index2) {
-  this.boards[index1].DOMNode.style.transition = "none";
-  this.boards[index2].DOMNode.style.transition = "none";
-  this.boards[index1].DOMNode.style.transform = "translateX(0)";
-  this.boards[index2].DOMNode.style.transform = "translateX(0)";
- }
- 
- removeBoards() {
-  const boardsHolder = document.getElementById("boards");
-  while (boardsHolder.children[0]) {
-   boardsHolder.removeChild(boardsHolder.children[0]);
+
+  removeBoards() {
+    const boardsHolder = document.getElementById("boards");
+    while (boardsHolder.children[0]) {
+      boardsHolder.removeChild(boardsHolder.children[0]);
+    }
   }
- }
- 
- drawBoards() {
-  this.removeBoards();
-  for (let i = 0; i < this.boards.length; i++) {
-   this.boards[i].draw();
+
+  drawBoards() {
+    this.removeBoards();
+    for (let i = 0; i < this.boards.length; i++) {
+      this.boards[i].draw();
+    }
   }
- }
- 
- createNewBoard(board) {
-  const { title, pickedColor: color, desc } = board;
-  const newBoard = new Board(title, color, desc, this.boards.length, null);
-  newBoard.init();
-  this.boards.push(newBoard);
-  this.drawBoards();
- }
- 
- swapBoards(i, j) {
-  [this.boards[i], this.boards[j]] = [this.boards[j], this.boards[i]];
- }
+
+  createNewBoard(board) {
+    const { title, pickedColor: color, desc } = board;
+    const newBoard = new Board(title, color, desc, this.boards.length, null);
+    newBoard.init();
+    this.boards.push(newBoard);
+    this.drawBoards();
+  }
+
+  swapBoards(i, j) {
+    [this.boards[i], this.boards[j]] = [this.boards[j], this.boards[i]];
+  }
 }
 
 export default Kanban;
